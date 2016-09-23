947a2cd5f280b2753ed4c4845a83861b931188a0
Shrujan Kotturi <skotturi@uncc.edu>
2016-09-22 23:40:26 -0400

    Sql file added

--- user.js
+++ user.js
@@ -12,13 +12,14 @@ function User() {
                 page = page %20;
             }
             limits = 50 * (page - 1);
-            con.query('SELECT * FROM Users ORDER BY ? ? LIMIT ? , ?', [sortby, orderby, limits, 50], function (err, result) {
+            var resultQuery = con.query('SELECT * FROM Users ORDER BY ? ? LIMIT ? , ?', [sortby, orderby, limits, 50], function (err, result) {
                 if(result.length != 0){
                     res.send(result);
                 }
                 else{
                     res.send({'status' : 'No Result'});
                 }
+                console.log(resultQuery);
             });
             con.release();
         });
